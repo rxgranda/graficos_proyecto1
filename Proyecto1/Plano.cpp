bb734988edf394389dfad69f6a5e9f56889f50bf
rxgranda <rxgranda@gmail.com>
2013-05-28 20:02:38 -0500

    Version Final

--- Proyecto1/Plano.cpp
+++ Proyecto1/Plano.cpp
@@ -1,53 +1,65 @@
-
+/**
+*Proyecto 1 Gráficos por Computadora
+*
+* Integrantes:
+*
+*   Gianni Carlo
+*   Roger Granda
+*   Denisse Pintado
+*
+*
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <GL/glut.h>
-#include <iostream>
-#include <windows.h>
-static GLfloat x_Spin=0.0;
-static GLfloat y_Spin=0.0;
-static GLfloat z_Spin=0.0;
-static GLfloat ancho_plano=500;
-static GLfloat alto_plano=500;
+
+GLfloat x_Spin=0.0f;
+GLfloat y_Spin=0.0f;
+GLfloat z_Spin=0.0f;
+GLfloat ancho_plano=500;
+GLfloat alto_plano=500;
 GLfloat xPosicion = 0.0f;
 GLfloat yPosicion = 0.0f;
-GLfloat yRotationAngle = 0.0f;
 
-
-static GLfloat pared[4][2]={
+GLfloat pared[][2]={
 	0.0,0.0,	
 	100.0,0.0,	
 	100.0,100.0,
 	0.0,100.0	
 };
-GLfloat ventanaI[4][2]={
+GLfloat ventanaI[][2]={
 	0.0,50.0,	
 	25.0,50.0,						
 	25.0,75.0,
 	0.0,75.0
 };
 
-GLfloat ventanaD[4][2]={
+GLfloat ventanaD[][2]={
 	75.0,50.0,	
 	100.0,50.0,						
 	100.0,75.0,
 	75.0,75.0
 };
 
-GLfloat puerta[4][2]={
+GLfloat puerta[][2]={
 	35.0,0.0,	
 	65.0,0.0,						
 	65.0,50.0,
 	35.0,50.0
 };
 
-GLfloat techo[4][2]={
+GLfloat techo[][2]={
 	0.0,100.0,	
 	100.0,100.0,						
 	50.0,150.0					
 };
 
-
+/*
+* Procedimiento: reshape
+* Parámetros: int w, int h
+* Descripción: Establece el worldspace, model, y viewport de la escena
+* Autor: Gianni Carlo, Roger Granda
+*/
 void reshape(int w, int h){
 	glViewport(0,0,(GLsizei)w,(GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
@@ -55,166 +67,174 @@ void reshape(int w, int h){
 	glOrtho(-ancho_plano/2,ancho_plano/2,-ancho_plano/2, ancho_plano/2, -ancho_plano/2, ancho_plano/2);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-
 }
 
-
-
+/*
+* Procedimiento: display
+* Parámetros: -
+* Descripción: Realiza los procesos geométricos del cuerpo (Rotación y traslación del cuerpo), y Rasterizado del cuerpo y el plano cartesiano 
+* Autores: Denisse Pintado, Roger Granda, Gianni Carlo
+*/
 void display (void){
 	glClear(GL_COLOR_BUFFER_BIT);
-	//glPushMatrix();
-
+	glLoadIdentity();
 	//Abscisa y Ordenada	
+	glLineWidth (5);
 	glBegin(GL_LINES);
-	glColor3f(1.0,1.0,1.0);
-	glVertex2f(-ancho_plano/2,0.0);
-	glVertex2f(ancho_plano/2,0.0);
-	glVertex2f(0.0,-ancho_plano/2);
-	glVertex2f(0.0,ancho_plano/2);
-
+		glColor3f(0.0,1.0,1.0);
+		glVertex2f(-ancho_plano/2,0.0);
+		glVertex2f(ancho_plano/2,0.0);
+		glVertex2f(0.0,-ancho_plano/2);
+		glVertex2f(0.0,ancho_plano/2);
 	glEnd();
-
 	//Grid
 	glLineStipple(2,0xAAAA);
 	glEnable(GL_LINE_STIPPLE);
-	glBegin(GL_LINES);
-	glColor3f(0.0,1.0,1.0);
-	//Grid Vertical		
-	for(int i=-ancho_plano/2;i<ancho_plano/2; i=i+ancho_plano/10){
-		if(i!=0.0){				
-			glVertex2f(i,alto_plano/2);
-			glVertex2f(i,-alto_plano/2);
+	glLineWidth (1);
+	glBegin(GL_LINES);	
+		glColor3f (0.5F, 0.5F, 0.5F);
+		//Grid Vertical		
+		for(int i=-ancho_plano/2;i<ancho_plano/2; i=i+ancho_plano/10){
+			if(i!=0.0){			
+				glVertex2f(i,alto_plano/2);
+				glVertex2f(i,-alto_plano/2);
+			}
 		}
-	}
-	//Grid Horizontal
-	for(int i=-alto_plano/2;i<alto_plano/2; i=i+alto_plano/10){
-		if(i!=0.0){
-			glVertex2f(-ancho_plano/2,i);
-			glVertex2f(ancho_plano/2,i);
+		//Grid Horizontal
+		for(int i=-alto_plano/2;i<alto_plano/2; i=i+alto_plano/10){
+			if(i!=0.0){
+				glVertex2f(-ancho_plano/2,i);
+				glVertex2f(ancho_plano/2,i);
+			}
 		}
-	}				
+
 	glEnd();
 	glDisable(GL_LINE_STIPPLE);	
-	glPushMatrix();
-	
+	glPushMatrix();	
 	glTranslatef(xPosicion,yPosicion,0.0);
 	glRotatef(x_Spin,1.0,0.0,0.0);
 	glRotatef(y_Spin,0.0,1.0,0.0);
 	glRotatef(z_Spin,0.0,0.0,1.0);
+	//Casa: Pared y ventanas
 	glBegin(GL_QUADS);
-	glColor3f(1.0,1.0,0.5);
-	for(int i=0;i<4;i++){
-		
-		glVertex2f(pared[i][0],pared[i][1]);
-		
-	}
-	glColor3f(1.0,0.0,0.0);
-	for(int i=0;i<4;i++){
-		glVertex2f(ventanaI[i][0],ventanaI[i][1]);	
-		
-	}
-	for(int i=0;i<4;i++){
-		glVertex2f(ventanaD[i][0],ventanaD[i][1]);	
-		
-	}
-	for(int i=0;i<4;i++){
-		glVertex2f(puerta[i][0],puerta[i][1]);
-		
-	}
-	
-
-	glEnd();	
+		glColor3f(1.0,1.0,0.5);
+		for(int i=0;i<4;i++){		
+			glVertex2f(pared[i][0],pared[i][1]);		
+		}
+		glColor3f(1.0,0.0,0.0);
+		for(int i=0;i<4;i++){
+			glVertex2f(ventanaI[i][0],ventanaI[i][1]);			
+		}
+		for(int i=0;i<4;i++){
+			glVertex2f(ventanaD[i][0],ventanaD[i][1]);			
+		}
+		for(int i=0;i<4;i++){
+			glVertex2f(puerta[i][0],puerta[i][1]);		
+		}	
+	glEnd();
+	//Casa: Techo
 	glBegin(GL_TRIANGLES);
-	glColor3f(1.0,0.0,1.0);
-	for(int i=0;i<4;i++){
-		glVertex2f(techo[i][0],techo[i][1]);		
-		
-	}
-	
+		glColor3f(1.0,0.0,1.0);
+		for(int i=0;i<4;i++){
+			glVertex2f(techo[i][0],techo[i][1]);				
+		}	
 	glEnd();	  	
 	glPopMatrix();
 	glutSwapBuffers();
 }
 
+/*
+* Procedimiento: init
+* Parámetros: -
+* Descripción: Iniciliza los parametros del worldspace
+* Autores: Denisse Pintado, Roger Granda, Gianni Carlo
+*/
 void init (void){
 	glClearColor(0.0,0.0,0.0,0.0);
 	glShadeModel(GL_FLAT);
+	//glDisable(GL_CULL_FACE);
 }
 
-
+/*
+* Procedimiento: keyboard
+* Parámetros: unsigned char key, int x, int y
+* Descripción: Procedimiento que realiza las operaciones aritméticas involucradas en la rotación del cuerpo
+* Autor: Roger Granda
+*/
 void keyboard( unsigned char key, int x, int y ){
 	switch ( key ) {
 		case 'q':
-			z_Spin=z_Spin+2.5;	
+			z_Spin=z_Spin+5.0;	
 			if (z_Spin>360.0)
 				z_Spin=z_Spin-360.0;								
 			break;
 		case 'e':									
-			z_Spin=z_Spin-2.5;	
+			z_Spin=z_Spin-5.0;	
 			if (z_Spin<-360)
 				z_Spin=z_Spin+360.0;					
 			break;
-		case 'w':
-			y_Spin=y_Spin+2.5;	
+		case 'a':
+			y_Spin=y_Spin+5.0;	
 			if (y_Spin>360.0)
 				y_Spin=y_Spin-360.0;								
 			break;
-		case 's':									
-			y_Spin=y_Spin-2.5;	
+		case 'd':									
+			y_Spin=y_Spin-5.0;	
 			if (y_Spin<-360)
 				y_Spin=y_Spin+360.0;					
 			break;
-		case 'a':
-			x_Spin=x_Spin+2.5;	
+		case 'w':
+			x_Spin=x_Spin+5.0;	
 			if (x_Spin>360.0)
 				x_Spin=x_Spin-360.0;								
 			break;
-		case 'd':									
-			x_Spin=x_Spin-2.5;	
+		case 's':									
+			x_Spin=x_Spin-5.0;	
 			if (x_Spin<-360)
 				x_Spin=x_Spin+360.0;					
 			break;
 		default:		
 			break;
 	}
-		glutPostRedisplay();
-	
+	glutPostRedisplay();	
 }
+
+/*
+* Procedimiento: handleSpecialKeypress
+* Parámetros: int key, int x, int y
+* Descripción: Procedimiento que realiza las operaciones aritméticas involucradas en la tranlación del cuerpo
+* Autor: Gianni Carlo
+*/
 void handleSpecialKeypress(int key, int x, int y) {
 	switch ( key ) {
-
-	case GLUT_KEY_UP:									
-		yPosicion=yPosicion+2.5;		
-		
-		break;
-	case GLUT_KEY_DOWN:											
-		
-		yPosicion=yPosicion-2.5;
-		break;
-	case GLUT_KEY_RIGHT:									
-		xPosicion=xPosicion+2.5;
-		
-		break;
-	case GLUT_KEY_LEFT:									
-		xPosicion=xPosicion-2.5;
-		
-		break;
-	default:									
-		break;
-
+		case GLUT_KEY_UP:									
+			yPosicion=yPosicion+5.0;				
+			break;
+		case GLUT_KEY_DOWN:													
+			yPosicion=yPosicion-5.0;
+			break;
+		case GLUT_KEY_RIGHT:									
+			xPosicion=xPosicion+5.0;		
+			break;
+		case GLUT_KEY_LEFT:									
+			xPosicion=xPosicion-5.0;		
+			break;
+		default:									
+			break;
 	}
 	glutPostRedisplay();
 }
 
-
+/*
+* Función: main
+* Parámetros: int argc, char** argv
+* Descripción: Punto de Inicio del programa
+* Autor: Denisse Pintado, Gianni Carlo, Roger Granda
+*/
 int main( int argc, char** argv )
 {
-	glutInit(&argc,argv);
-
-	// The image is not animated so single buffering is OK. 
+	glutInit(&argc,argv); 
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB );
-
-	// Window position (from top corner), and size (width and hieght)
 	glutInitWindowPosition( 100, 100 );
 	glutInitWindowSize( ancho_plano,alto_plano );
 	glutCreateWindow( "Proyecto 1");
@@ -222,9 +242,7 @@ int main( int argc, char** argv )
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
-	glutSpecialFunc(handleSpecialKeypress);
-	//glutMouseFunc(mouse);
+	glutSpecialFunc(handleSpecialKeypress);	
 	glutMainLoop();
-
 	return 0;	
 }
